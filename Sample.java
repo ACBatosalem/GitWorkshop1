d040d9dca2c2dbf5217c91a8810e5c71d99ea8a5
student <student@G302BPC10.manila.dlsu.edu.ph>
2018-03-02 17:27:07 +0800

    First Push

---
+++ Sample.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Sample.java
+ *
+ *
+ * @author 
+ * @version 1.00 2018/3/2
+ */
+
+
+public class Sample {
+
+    public static void main(String[] args){
+    	System.out.println("hello");
+    }
+    
+    
+}
