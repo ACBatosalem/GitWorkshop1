000a9d42dc7f793518382f5e00d6b3243aa1de9a
diane1998 <student@G302BPC04.manila.dlsu.edu.ph>
2018-03-02 17:56:50 +0800

    with commment

--- Sample.java
+++ Sample.java
@@ -1,5 +1,8 @@
-public class Sample {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+public class Sample{
+	public static void main(String [] args)
+	{
+	 	System.out.println("DEAD");
+		//----------------------------------------------
+	}
 }
+
